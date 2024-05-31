f228176e774dd8ba7fd7159393ef2dfb64a3487b
Helio Albano de Oliveira <helioalb@gmail.com>
2024-05-31 10:55:39 -0300

    Add authentication

--- auth.ts
+++ auth.ts
@@ -18,26 +18,29 @@ async function getUser(email: string): Promise<User | undefined> {
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [Credentials({
-    async authorize(credentials) {
-      const parsedCredentials = z.object({
-        email: z.string().email(),
-        password: z.string().min(6)
-      }).safeParse(credentials);
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z.object({
+          email: z.string().email(),
+          password: z.string().min(6)
+        }).safeParse(credentials);
 
-      if (parsedCredentials.success) {
-        const { email, password } = parsedCredentials.data;
-        const user = await getUser(email);
-        if (!user) return null;
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
 
-        const passwordMatch = await bcrypt.compare(password, user.password);
+          const passwordMatch = await bcrypt.compare(password, user.password);
 
-        if (passwordMatch) {
-          return user;
+          if (passwordMatch) {
+            return user;
+          }
         }
-      }
 
-      console.log('Invalid credentials');
-      return null;
-  })],
-})
+        console.log('Invalid credentials');
+        return null;
+      }
+    })
+  ]
+});
